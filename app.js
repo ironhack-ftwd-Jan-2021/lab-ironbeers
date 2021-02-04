202b9d52e066d56dc575cb4f09fd1c6eb1dcbfef
ivan-user <username.web@protonmail.com>
2021-02-04 16:23:05 +0100

    First commit

--- app.js
+++ app.js
@@ -13,13 +13,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+const handlebars = require('hbs');
+handlebars.registerPartials(__dirname + '/views/partials');
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',);
 });
+
+
+
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beers', (req, res) => {
+    
+        const randomBeer = punkAPI.getRandom()
+        randomBeer.then(beer => {
+            alert(beer[0].name)
+      // your magic happens here
+    })
+    .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
